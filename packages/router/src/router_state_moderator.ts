9c8b70bd7a0ad83ff52affa98bcf72dc5cb3bcfb
Andrew Scott <atscott@google.com>
2022-11-14 11:37:47 -0800

    fixup! create url state file as a fork of router

--- packages/router/src/router_state_moderator.ts
+++ packages/router/src/router_state_moderator.ts
@@ -1,11 +1,46 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs';
 
-import {EventType} from './events';
+import {NavigationCancel, NavigationError} from './events';
 import {NavigationTransition} from './router';
 
-interface NavigationEvent {
-  type: EventType;
+export enum RouterStateEventType {
+  NavigationStart,
+  NavigationEnd,
+  RoutesRecognized,
+  NavigationCancel,
+  NavigationError,
+  OutletActivationStart,
+}
+
+export type NavigationEvent =
+    ModeratorNavigationCancel|ModeratorNavigationError|ModeratorNavigationEnd|
+    ModeratorNavigationStart|ModeratorRoutesRecognized|ModeratorOutletActivationStart;
+
+export interface ModeratorNavigationCancel {
+  type: RouterStateEventType.NavigationCancel;
+  navigation: NavigationTransition;
+  routerEvent: NavigationCancel;
+}
+export interface ModeratorNavigationError {
+  type: RouterStateEventType.NavigationError;
+  navigation: NavigationTransition;
+  routerEvent: NavigationError;
+}
+export interface ModeratorNavigationStart {
+  type: RouterStateEventType.NavigationStart;
+  navigation: NavigationTransition;
+}
+export interface ModeratorNavigationEnd {
+  type: RouterStateEventType.NavigationEnd;
+  navigation: NavigationTransition;
+}
+export interface ModeratorRoutesRecognized {
+  type: RouterStateEventType.RoutesRecognized;
+  navigation: NavigationTransition;
+}
+export interface ModeratorOutletActivationStart {
+  type: RouterStateEventType.OutletActivationStart;
   navigation: NavigationTransition;
 }
 
