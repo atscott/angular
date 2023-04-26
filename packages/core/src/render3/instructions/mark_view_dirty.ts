41ebd4a182ab91bdab79865c545926b1a538637b
Andrew Scott <atscott@google.com>
2023-04-26 16:47:02 -0700

    update markViewDirty to do the correct thing for signal views

--- packages/core/src/render3/instructions/mark_view_dirty.ts
+++ packages/core/src/render3/instructions/mark_view_dirty.ts
@@ -9,6 +9,7 @@
 import {isRootView} from '../interfaces/type_checks';
 import {FLAGS, LView, LViewFlags} from '../interfaces/view';
 import {getLViewParent} from '../util/view_traversal_utils';
+import {markViewForRefresh} from '../util/view_utils';
 
 /**
  * Marks current view and all ancestors dirty.
@@ -21,16 +22,35 @@ import {getLViewParent} from '../util/view_traversal_utils';
  * @param lView The starting LView to mark dirty
  * @returns the root LView
  */
-export function markViewDirty(lView: LView): LView|null {
+export function markViewDirty(initialLView: LView): LView|null {
+  if (initialLView[FLAGS] & LViewFlags.SignalView) {
+    markViewForRefresh(initialLView);
+    return null;
+  }
+
+  let lView: LView|null = initialLView;
   while (lView) {
-    lView[FLAGS] |= LViewFlags.Dirty;
-    const parent = getLViewParent(lView);
-    // Stop traversing up as soon as you find a root view that wasn't attached to any container
-    if (isRootView(lView) && !parent) {
-      return lView;
+    if ((lView[FLAGS] & LViewFlags.SignalView)) {
+      // We do not mark ancestor signal views dirty. We should skip over them and keep marking any
+      // Zone-based views higher up as Dirty.
+      lView = getLViewParent(lView);
+    } else {
+      lView[FLAGS] |= LViewFlags.Dirty;
+      const parent = getLViewParent(lView);
+      // Stop traversing up as soon as you find a root view that wasn't attached to any container
+      if (isRootView(lView) && !parent) {
+        return lView;
+      }
+
+      // If the parent of this view is a signal view, we aren't going to be marking it as dirty.
+      // That means we need to mark _this_ view with the refresh flag that is capable of 'piercing'
+      // non-dirty ancestors
+      if (parent && parent[FLAGS] & LViewFlags.SignalView) {
+        markViewForRefresh(lView);
+      }
+
+      lView = parent;
     }
-    // continue otherwise
-    lView = parent!;
   }
   return null;
 }
